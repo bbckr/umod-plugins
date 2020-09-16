f21108ab07668642fffcf9a2846f79c2a45f9662
unknown <bri@bckr.io>
2020-09-16 13:14:36 +0200

    Test if player names in server query are anonymized

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/bbckr/umod-plugins
 
 go 1.15
+
+require (
+	github.com/rumblefrog/go-a2s v1.0.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.6.1
+)
--- tests/plugins_test.go
+++ tests/plugins_test.go
@@ -1,9 +1,37 @@
 package tests
 
 import (
+	"os"
 	"testing"
+
+	"github.com/rumblefrog/go-a2s"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	CONNECT_IP      = os.Getenv("CONNECT_IP")
+	ANONYMIZED_NAME = "StreamerFriendly"
 )
 
 func Test_StreamerFriendly(t *testing.T) {
+	client, err := a2s.NewClient(CONNECT_IP)
+	if err != nil {
+		panic(err)
+	}
 
+	playerInfo, err := client.QueryPlayer()
+	if err != nil {
+		panic(err)
+	}
+
+	t.Run("player names in server query are anonymized", func(t *testing.T) {
+
+		if len(playerInfo.Players) == 0 {
+			assert.Fail(t, "unable to test plugin due to absent player count")
+		}
+
+		for _, player := range playerInfo.Players {
+			assert.Equal(t, ANONYMIZED_NAME, player.Name)
+		}
+	})
 }
