9469db6d23588af1016238f9846d65e31d0467ae
unknown <bri@bckr.io>
2020-09-16 12:49:15 +0200

    Plugin boilerplate for StreamerFriendly

---
+++ plugins/StreamerFriendly.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Oxide.Core;
+using Oxide.Core.Libraries.Covalence;
+using Steamworks;
+
+namespace Oxide.Plugins
+{
+    [Info("StreamerFriendly", "bbckr", "0.1.0")]
+    [Description("")]
+    class StreamerFriendly : RustPlugin
+    {
+
+    }
+}
