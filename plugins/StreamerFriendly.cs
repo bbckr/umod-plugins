7980ed6b556b54077b4ffc9974b70f37d9fecd25
unknown <bri@bckr.io>
2020-09-27 11:35:01 +0200

    Start up own rcon server for sending messages

--- plugins/StreamerFriendly.cs
+++ plugins/StreamerFriendly.cs
@@ -1,29 +1,55 @@
 using System;
 using System.Collections.Generic;
+using WebSocketSharp.Net.WebSockets;
+using Oxide.Core;
+using Oxide.Core.Configuration;
 using Oxide.Core.Libraries.Covalence;
+using Oxide.Core.RemoteConsole;
 using Steamworks;
-using Facepunch;
-using Network.Visibility;
+using WebSocketSharp.Server;
+using Newtonsoft.Json;
+using WebSocketSharp;
+using System.Net;
+
+/*
+ * TODO
+ * 
+ * - Check if RCON service already running (either abort or run on another port)
+ * - Add more hooks
+ * - Language variations, dictionary for hook messages
+ * - Break out into seperate plugin and have streamer friendly use
+ * 
+ */
 
 namespace Oxide.Plugins
 {
     [Info("StreamerFriendly", "bbckr", "0.2.0")]
-    [Description("A plugin that prevents external services from tracking players via Steam Queries.")]
+    [Description("A plugin that prevents external services from tracking players via Steam Queries and RCON.")]
     class StreamerFriendly : RustPlugin
     {
-        private static Anonymizer anonymizer = new Anonymizer();
-        private static RconListener rconListener = new RconListener();
+        private Anonymizer anonymizer = new Anonymizer();
+        private AnonymizedRemoteConsole anonymizedRemoteConsole = new AnonymizedRemoteConsole();
 
         void Loaded()
         {
-            // Start listening to rcon
-            rconListener.Subscribe();
+            // Anonymize rcon messages
+            anonymizedRemoteConsole.Start();
 
             // Anonymize player info
             var activeBasePlayers = BasePlayer.activePlayerList;
             for (int i = 0; i < activeBasePlayers.Count; i++) {
                 anonymizer.Anonymize(activeBasePlayers[i].IPlayer);
             }
+        }
+
+        object OnPlayerDeath(BasePlayer player, HitInfo info)
+        {
+            // TODO: (bckr) imitate console message
+            anonymizedRemoteConsole.SendMessage(new RemoteMessage { 
+                Message = "they died",
+                Type = "Generic"
+            });
+            return null;
         }
 
         void OnUserConnected(IPlayer player)
@@ -40,8 +66,8 @@ namespace Oxide.Plugins
 
         void Unload()
         {
-            // Must unsubscribe OR ELSE
-            rconListener.Unsubscribe();
+            // Stop rcon console
+            anonymizedRemoteConsole.Stop();
 
             // Deanonymize player info
             var activeBasePlayers = BasePlayer.activePlayerList;
@@ -51,28 +77,104 @@ namespace Oxide.Plugins
             }
         }
 
-        private class RconListener
+        private class AnonymizedRemoteConsole
         {
-            private Action<string, string, UnityEngine.LogType> CustomOnMessage;
+            private WebSocketServer server;
+            private AnonymizedWebSocketBehavior behavior;
 
-            public RconListener()
+            private readonly int port;
+            private readonly string password;
+
+            public AnonymizedRemoteConsole()
             {
-                CustomOnMessage = new Action<string, string, UnityEngine.LogType>(OnMessage);
+                port = Interface.Oxide.Config.Rcon.Port;
+                password = Interface.Oxide.Config.Rcon.Password;
             }
 
-            public void Subscribe()
+            public void Start()
             {
-                Output.OnMessage += CustomOnMessage;
+                if (server != null && behavior != null)
+                {
+                    Interface.Oxide.LogWarning("rcon server already started");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(password))
+                {
+                    Interface.Oxide.LogWarning("rcon server is unprotected: it is recommended a password is set");
+                }
+
+                try
+                {
+                    server = new WebSocketServer(port) { WaitTime = TimeSpan.FromSeconds(5.0), ReuseAddress = true };
+                    server.AddWebSocketService(string.Format("/{0}", password), () => behavior = new AnonymizedWebSocketBehavior(this));
+
+                    server.Start();
+                }
+                catch (Exception exception)
+                {
+                    Interface.Oxide.LogException($"rcon server failed to initialize", exception);
+                    return;
+                }
+
+                Interface.Oxide.LogInfo("rcon server listening on {0}", server.Port);
             }
 
-            public void Unsubscribe()
+            public void Stop()
             {
-                Output.OnMessage -= CustomOnMessage;
+                if (server != null && !server.IsListening)
+                {
+                    server.Stop();
+                    server = null;
+                    behavior = null;
+                    Interface.Oxide.LogInfo($"rcon server has stopped");
+                }
             }
 
-            private void OnMessage(string arg0, string arg1, UnityEngine.LogType arg3)
+            public void SendMessage(RemoteMessage message)
             {
+                // TODO: (bckr) anonymize message here
+                var serializedMessage = JsonConvert.SerializeObject(message, Formatting.Indented);
+                server.WebSocketServices.Broadcast(serializedMessage);
+            }
 
+            private void OnMessage(MessageEventArgs e)
+            {
+                // TODO: (bckr) make MessageEventArgs to serialized remotemessage
+                server.WebSocketServices.Broadcast(e.Data);
+            }
+
+            private class AnonymizedWebSocketBehavior : WebSocketBehavior
+            {
+                private readonly AnonymizedRemoteConsole Parent;
+                private IPAddress _address;
+
+                public AnonymizedWebSocketBehavior(AnonymizedRemoteConsole parent)
+                {
+                    Parent = parent;
+                    IgnoreExtensions = true;
+                }
+
+                protected override void OnClose(CloseEventArgs e)
+                {
+                    Interface.Oxide.LogInfo("rcon connection {0} closed", _address);
+                }
+
+                protected override void OnError(ErrorEventArgs e)
+                {
+                    Interface.Oxide.LogException(string.Format("rcon exception: {0}", e.Message), e.Exception);
+                }
+
+                protected override void OnMessage(MessageEventArgs e)
+                {
+                    Parent?.OnMessage(e);
+                }
+
+                protected override void OnOpen()
+                {
+                    _address = Context.UserEndPoint.Address;
+                    Interface.Oxide.LogInfo("rcon connection {0} established", _address);
+                }
             }
         }
 
